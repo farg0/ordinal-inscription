a671a803ccf0e8fddceddbd6fc816f2912483072
dom <dom@gmail.com>
2024-04-02 11:41:55 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
-const { createTextInscription } = require('./ordinals-bitcoinjs');
+const {
+  ECPair,
+  createTextInscription,
+  createCommitTxData,
+  createRevealTx,
+} = require('./ordinals-bitcoinjs');
+const bitcoinjs = require('bitcoinjs-lib');
 
-const ins = createTextInscription('test123', 100);
-console.log(ins);
+const secret = '';
+const toAddress = 'tb1q5eja48z06hcgdqtmahxhegdp8nys84997szfjf';
+const inputTxId =
+  'af348cf9eac6b2a25000873549bb7ec1bdf5ec674c4212accd03778259c26355';
+const inputIndex = 4;
+
+async function main() {
+  const privateKey = Buffer.from(secret, 'hex');
+  const keypair = ECPair.fromPrivateKey(privateKey);
+  const publicKey = keypair.publicKey;
+
+  const inscription = createTextInscription({ text: 'Hello!!' });
+  const commitTxData = createCommitTxData({
+    publicKey,
+    inscription,
+  });
+
+  const padding = 549;
+  const txSize = 600 + Math.floor(inscription.content.length / 4);
+  const feeRate = 2;
+  const minersFee = txSize * feeRate;
+
+  const requiredAmount = 550 + minersFee + padding;
+
+  const commitTxResult = {
+    txId: inputTxId,
+    sendUtxoIndex: inputIndex,
+    sendAmount: requiredAmount,
+  };
+
+  const revelRawTx = await createRevealTx({
+    commitTxData,
+    commitTxResult,
+    toAddress,
+    privateKey,
+    amount: padding,
+  });
+  console.log(` create reveal rawtx ${revelRawTx.rawTx}`);
+}
+
+main().catch();
--- src/ordinals-bitcoinjs.js
+++ src/ordinals-bitcoinjs.js
@@ -1,35 +1,42 @@
-const psbtUtils = require('bitcoinjs-lib/src/psbt/psbtutils')
-const ECPairFactory = require('ecpair')
-const assert = require('minimalistic-assert')
-const bitcoinjsLib = require('bitcoinjs-lib')
-const ecc = require('tiny-secp256k1')
+const psbtUtils = require('bitcoinjs-lib/src/psbt/psbtutils');
+const ECPairFactory = require('ecpair');
+const assert = require('minimalistic-assert');
+const bitcoinjsLib = require('bitcoinjs-lib');
+const ecc = require('tiny-secp256k1');
 
-const { witnessStackToScriptWitness } = psbtUtils
+const { witnessStackToScriptWitness } = psbtUtils;
 
-bitcoinjsLib.initEccLib(ecc)
+bitcoinjsLib.initEccLib(ecc);
 
-const ECPair = ECPairFactory.default(ecc)
+const ECPair = ECPairFactory.default(ecc);
 
-const network = bitcoinjsLib.networks.bitcoin
+const network = bitcoinjsLib.networks.testnet;
+// const network = bitcoinjsLib.networks.bitcoin;
 
-const encoder = new TextEncoder()
+const encoder = new TextEncoder();
 
 function toXOnly(pubkey) {
-  return pubkey.subarray(1, 33)
+  return pubkey.subarray(1, 33);
 }
 
 function createTextInscription({ text, postage = 10000 }) {
-  const contentType = Buffer.from(encoder.encode('text/plain;charset=utf-8'))
-  const content = Buffer.from(encoder.encode(text))
-  return { contentType, content, postage }
+  const contentType = Buffer.from(encoder.encode('text/plain;charset=utf-8'));
+  const content = Buffer.from(encoder.encode(text));
+  return { contentType, content, postage };
 }
 
 function createInscriptionScript({ xOnlyPublicKey, inscription }) {
-  assert(xOnlyPublicKey instanceof Buffer, `xOnlyPublicKey must be a Buffer`)
-  assert(inscription, `inscription is required`)
-  assert(inscription.content instanceof Buffer, `inscription.content must be a Buffer`)
-  assert(inscription.contentType instanceof Buffer, `inscription.content must be a Buffer`)
-  const protocolId = Buffer.from(encoder.encode('ord'))
+  assert(xOnlyPublicKey instanceof Buffer, `xOnlyPublicKey must be a Buffer`);
+  assert(inscription, `inscription is required`);
+  assert(
+    inscription.content instanceof Buffer,
+    `inscription.content must be a Buffer`
+  );
+  assert(
+    inscription.contentType instanceof Buffer,
+    `inscription.content must be a Buffer`
+  );
+  const protocolId = Buffer.from(encoder.encode('ord'));
   return [
     xOnlyPublicKey,
     bitcoinjsLib.opcodes.OP_CHECKSIG,
@@ -43,34 +50,35 @@ function createInscriptionScript({ xOnlyPublicKey, inscription }) {
     bitcoinjsLib.opcodes.OP_0,
     inscription.content,
     bitcoinjsLib.opcodes.OP_ENDIF,
-  ]
+  ];
 }
 
 function createCommitTxData({ publicKey, inscription }) {
-  assert(publicKey, 'encodePublic is required')
-  assert(inscription, 'inscription is required')
-  const xOnlyPublicKey = toXOnly(publicKey)
-  const script = createInscriptionScript({ xOnlyPublicKey, inscription })
+  assert(publicKey, 'encodePublic is required');
+  assert(inscription, 'inscription is required');
+  const xOnlyPublicKey = toXOnly(publicKey);
+  const script = createInscriptionScript({ xOnlyPublicKey, inscription });
 
-  const outputScript = bitcoinjsLib.script.compile(script)
+  const outputScript = bitcoinjsLib.script.compile(script);
 
   const scriptTree = {
     output: outputScript,
     redeemVersion: 192,
-  }
+  };
 
   const scriptTaproot = bitcoinjsLib.payments.p2tr({
     internalPubkey: xOnlyPublicKey,
     scriptTree,
     redeem: scriptTree,
     network,
-  })
+  });
 
-  const tapleaf = scriptTaproot.hash.toString('hex')
+  const tapleaf = scriptTaproot.hash.toString('hex');
 
-  const revealAddress = scriptTaproot.address
-  const tpubkey = scriptTaproot.pubkey.toString('hex')
-  const cblock = scriptTaproot.witness?.[scriptTaproot.witness.length - 1].toString('hex')
+  const revealAddress = scriptTaproot.address;
+  const tpubkey = scriptTaproot.pubkey.toString('hex');
+  const cblock =
+    scriptTaproot.witness?.[scriptTaproot.witness.length - 1].toString('hex');
 
   return {
     script,
@@ -80,62 +88,72 @@ function createCommitTxData({ publicKey, inscription }) {
     revealAddress,
     scriptTaproot,
     outputScript,
-  }
+  };
 }
 
-async function createRevealTx({ commitTxData, commitTxResult, toAddress, privateKey, amount }) {
-  assert(commitTxData, `commitTxData is required`)
-  assert(commitTxResult, `commitTxResult is required`)
-  assert(toAddress, `toAddress is required`)
-  assert(privateKey instanceof Buffer, `privateKey must be a Buffer`)
-  assert(typeof amount === 'number', `amount must be a number`)
+async function createRevealTx({
+  commitTxData,
+  commitTxResult,
+  toAddress,
+  privateKey,
+  amount,
+}) {
+  assert(commitTxData, `commitTxData is required`);
+  assert(commitTxResult, `commitTxResult is required`);
+  assert(toAddress, `toAddress is required`);
+  assert(privateKey instanceof Buffer, `privateKey must be a Buffer`);
+  assert(typeof amount === 'number', `amount must be a number`);
 
-  const { cblock, scriptTaproot, outputScript } = commitTxData
+  const { cblock, scriptTaproot, outputScript } = commitTxData;
 
   const tapLeafScript = {
     leafVersion: scriptTaproot.redeemVersion, // 192 0xc0
     script: outputScript,
     controlBlock: Buffer.from(cblock, 'hex'),
-  }
+  };
 
-  const keypair = ECPair.fromPrivateKey(privateKey, { network })
-  const psbt = new bitcoinjsLib.Psbt({ network })
+  const keypair = ECPair.fromPrivateKey(privateKey, { network });
+  const psbt = new bitcoinjsLib.Psbt({ network });
   psbt.addInput({
     hash: commitTxResult.txId,
     index: commitTxResult.sendUtxoIndex,
-    witnessUtxo: { value: commitTxResult.sendAmount, script: scriptTaproot.output },
+    witnessUtxo: {
+      value: commitTxResult.sendAmount,
+      script: scriptTaproot.output,
+    },
     tapLeafScript: [tapLeafScript],
-  })
+  });
 
   psbt.addOutput({
     value: amount, // generally 1000 for nfts, 549 for brc20
     address: toAddress,
-  })
+  });
 
-  await psbt.signInput(0, keypair)
+  psbt.signInput(0, keypair);
 
-  const signature = psbt.data.inputs[0].tapScriptSig[0].signature.toString('hex')
+  const signature =
+    psbt.data.inputs[0].tapScriptSig[0].signature.toString('hex');
 
   // We have to construct our witness script in a custom finalizer
 
   const customFinalizer = (_inputIndex, input) => {
     const witness = [input.tapScriptSig[0].signature]
       .concat(outputScript)
-      .concat(tapLeafScript.controlBlock)
+      .concat(tapLeafScript.controlBlock);
 
     return {
       finalScriptWitness: witnessStackToScriptWitness(witness),
-    }
-  }
+    };
+  };
 
-  psbt.finalizeInput(0, customFinalizer)
+  psbt.finalizeInput(0, customFinalizer);
 
-  const tx = psbt.extractTransaction()
+  const tx = psbt.extractTransaction();
 
-  const rawTx = tx.toBuffer().toString('hex')
-  const txId = tx.getId()
+  const rawTx = tx.toBuffer().toString('hex');
+  const txId = tx.getId();
 
-  const virtualSize = tx.virtualSize()
+  const virtualSize = tx.virtualSize();
 
   return {
     txId,
@@ -143,7 +161,7 @@ async function createRevealTx({ commitTxData, commitTxResult, toAddress, private
     inscriptionId: `${txId}i0`,
     virtualSize,
     signature,
-  }
+  };
 }
 
 module.exports = {
@@ -153,4 +171,4 @@ module.exports = {
   ECPair,
   witnessStackToScriptWitness,
   toXOnly,
-}
+};
